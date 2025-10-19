cd26fe7308d01bf54e63c6fbb38a62d953083bd3
mallowwww <mallowhorton@proton.me>
2025-10-18 20:43:12 -0500

    Initial commit

---
+++ web.lua
@@ -0,0 +1,23 @@
+local serverLocation = settings.get("server.path")
+if not serverLocation or type(serverLocation) ~= "string" then
+    print("ERROR - Server module path not set !")
+end
+local server = require(serverLocation)
+if not server then
+    print("ERROR - Could not load server module !")
+end
+
+local app = server.new(80)
+local directory = shell.dir()
+
+app:listen("/", "GET", function(pack)
+    local handle = fs.open(directory .. "/index.ccml", "r")
+    local status = 200
+    local body = ""
+    if not handle then status = 404 end
+    if handle then
+        body = handle.readAll()
+    end
+    
+    return {body = "", contentType = "text/plain", status = 200}
+end)
